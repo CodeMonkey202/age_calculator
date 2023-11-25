ffe2a7a94f41733f35fcdfaf8fcd2a611d4f5894
CodeMonkey202olamide2004#git config --global user.name CodeMonkey202 <olateju202@gmail.com>
2023-11-25 22:27:49 +0100

    first commit 2021 Noveber Coomit in the Night with Daniel listening to Arome Osayi Message

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello{
+    public static void main(String args[]){
+        System.out.println("YUSUF AYORINDE RILWAN"); //author name
+       System.out.println("AU21AC3782"); //author matric number
+       System.out.println("VISUAL STUDIO CODE"); //author IDE
+       System.out.println("Step 1: Go to chrome");
+       System.out.println("Step 2: Go to the official V java IDE website");
+       System.out.println("Step 3: Click on download section ");
+       System.out.println("Step 4: Once the downloadlink for the versionof VC java IDE that want to install");
+       System.out.println("Step 5: Once the download link for is complete, locate the download file on your computer");
+       System.out.println("Step 6: Double-click on the file to start the installation process");
+       
+    }
+}
